e8cf416352f61c17c87928752cfac7f9541bfbef
Amalth V H <amalthvh@gmail.com>
2026-07-29 00:08:45 +0530

    Fix Render ALLOWED_HOSTS

--- archiveXstudio/settings.py
+++ archiveXstudio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(^#gf5+($x4k4@u)0eudk&i%l!r1ha2g+)v67*mh%pq*&wzt&5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['archivexstudio.onrender.com']
 
 
 # Application definition
